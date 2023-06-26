70a0d876bd788bf11629191f7732ed0a158f4f54
JinsmonJoseph <jinzzjneendusseril777@gmail.com>
2023-06-25 20:02:04 -0400

    created contact model

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@router GET /api/contacts
 //@access public
 
 const getContacts= asyncHandler(async(req,res)=>{
-    res.status(200).json({message:"Get all contacts"});
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
 });
 
 //@desc Get a contact with id
@@ -20,8 +22,18 @@ const getContact = asyncHandler (async (req,res)=>{
 //@access public
 
 const createContact = asyncHandler (async (req,res)=>{
-    console.log("The request body is : ",req.body);
-    res.status(200).json({message:"Create a contact"});
+    console.log("The request body is :", req.body);
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+      res.status(400);
+      throw new Error("All fields are mandatory !");
+    }
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+      });
+    res.status(200).json(contact);
 });
 //@desc Update a contact with id
 //@router POST /api/contacts/;id
