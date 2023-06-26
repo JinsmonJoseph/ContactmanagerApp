02dee38402c122bff0f5679980e686bc0e74e4e6
JinsmonJoseph <jinzzjneendusseril777@gmail.com>
2023-06-25 20:52:22 -0400

    configured login route in controller

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -37,4 +37,34 @@ const registerUser = asyncHandler(async (req, res) => {
     }
   });
 
-  module.exports = { registerUser};
+//@desc Login user
+//@route POST /api/users/login
+//@access public
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400);
+      throw new Error("All fields are mandatory!");
+    }
+    const user = await User.findOne({ email });
+    //compare password with hashedpassword
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const accessToken = jwt.sign(
+        {
+          user: {
+            username: user.username,
+            email: user.email,
+            id: user.id,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECERT,
+        { expiresIn: "10m" }
+      );
+      res.status(200).json({ accessToken });
+    } else {
+      res.status(401);
+      throw new Error("email or password is not valid");
+    }
+  });
+
+  module.exports = { registerUser , loginUser};
--- mycontacts-backend/routes/userRoutes.js
+++ mycontacts-backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const {
-    registerUser
+    registerUser,
+    loginUser
   } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+
+router.post("/login", loginUser);
 
 
 module.exports = router;
